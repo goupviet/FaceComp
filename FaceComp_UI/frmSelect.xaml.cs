af7c9023a24bb9172628e4cf6f2f7b5d57514ec8
vohungvi <vohungvi27@gmail.com>
2020-12-25 14:46:09 +0700

    + add file

---
+++ FaceComp_UI/frmSelect.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FaceComp
+{
+    /// <summary>
+    /// Interaction logic for frmSelect.xaml
+    /// </summary>
+    public partial class frmSelect : Window
+    {
+        public frmSelect()
+        {
+            InitializeComponent();
+#if x64
+            this.Title += " x64";
+#else
+            this.Title += " x86";
+#endif
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            frmCompare frm = new frmCompare();
+            frm.Closed += Frm_Closed;
+            frm.ShowDialog();            
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            frmSearch frm = new frmSearch();
+            frm.Closed += Frm_Closed;
+            frm.ShowDialog();
+            
+        }
+
+        private void Frm_Closed(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void Label_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://viscomsolution.com/facecomp-phan-mem-so-sanh-khuon-mat/");
+        }
+    }
+}
